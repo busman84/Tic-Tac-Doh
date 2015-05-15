02409ac277180797f37ad3d720072400e49c9905
Matt Duncan <busman84@gmail.com>
2015-05-15 09:49:37 -0500

    working game

--- js/main.js
+++ js/main.js
@@ -14,44 +14,58 @@
 $(document).ready(function() {
     var x = "x"
     var o = "o"
-    var count = 0;
-    var o_win = 0;
-    var x_win = 0;
+    var turn = 0;
+    var benderWin = 0;
+    var homerWin = 0;
+
     $('.gameboard').click(function() {
 
+
         if ($(this).hasClass('disable')) {
+            turn--;
             alert("DOH!!! This space is already taken");
-        } else if (count == 9) {
-            alert("Cats Game!!!!");
-        } else if (count % 2 == 0) {
-            count++;
-            $(this).text(o);
-            $(this).addClass("disable o btn_o");
-            if ($('#squareTL').hasClass('o') && $('#squareMT').hasClass('o') && $('#squareTR').hasClass('o') || $('#squareML').hasClass('o') && $('#squareM').hasClass('o') && $('#squareMR').hasClass('o') || $('#squareBL').hasClass('o') && $('#squareML').hasClass('o') && $('#squareBR').hasClass('o') || $('#squareTL').hasClass('o') && $('#squareML').hasClass('o') && $('#squareBL').hasClass('o') || $('#squareMT').hasClass('o') && $('#squareM').hasClass('o') && $('#squareBM').hasClass('o') || $('#squareTR').hasClass('o') && $('#squareMR').hasClass('o') && $('#squareBR').hasClass('o') || $('#squareTL').hasClass('o') && $('#squareM').hasClass('o') && $('#squareBR').hasClass('o') || $('#squareTR').hasClass('o') && $('#squareM').hasClass('o') && $('#squareBL').hasClass('o')) {
-                alert("O Wins!!!");
-                o_win++;
-                $('#o_win').text(o_win);
+        } else if (turn % 2 == 0) {
+
+            $(this).html('<img src="img/bender2.png" class="pics">');
+            $(this).addClass("disable o");
+            if ($('#squareTL').hasClass('o') && $('#squareTM').hasClass('o') && $('#squareTR').hasClass('o') || $('#squareML').hasClass('o') && $('#squareM').hasClass('o') && $('#squareMR').hasClass('o') || $('#squareBL').hasClass('o') && $('#squareBM').hasClass('o') && $('#squareBR').hasClass('o') || $('#squareTL').hasClass('o') && $('#squareML').hasClass('o') && $('#squareBL').hasClass('o') || $('#squareTM').hasClass('o') && $('#squareM').hasClass('o') && $('#squareBM').hasClass('o') || $('#squareTR').hasClass('o') && $('#squareMR').hasClass('o') && $('#squareBR').hasClass('o') || $('#squareTL').hasClass('o') && $('#squareM').hasClass('o') && $('#squareBR').hasClass('o') || $('#squareTR').hasClass('o') && $('#squareM').hasClass('o') && $('#squareBL').hasClass('o')) {
+                alert(" Bite my shine metal ass!!!");
+                benderWin++;
+                $('#benderWin').text(benderWin);
+                $('.gameboard').text('');
+                $('.gameboard').removeClass('disable');
+                $('.gameboard').removeClass('o');
+                $('.gameboard').removeClass('x');
+                turn = 0;
 
             }
-        } else if (count % 2 != 0) {
-            count++;
-            $(this).text(x);
-            $(this).addClass("disable x btn_x");
-            if ($('#squareTL').hasClass('x') && $('#squareMT').hasClass('x') && $('#squareTR').hasClass('x') || $('#squareML').hasClass('x') && $('#squareM').hasClass('x') && $('#squareMR').hasClass('x') || $('#squareBL').hasClass('x') && $('#squareML').hasClass('x') && $('#squareBR').hasClass('x') || $('#squareTL').hasClass('x') && $('#squareML').hasClass('x') && $('#squareBL').hasClass('x') || $('#squareMT').hasClass('x') && $('#squareM').hasClass('x') && $('#squareBM').hasClass('x') || $('#squareTR').hasClass('x') && $('#squareMR').hasClass('x') && $('#squareBR').hasClass('x') || $('#squareTL').hasClass('x') && $('#squareM').hasClass('x') && $('#squareBR').hasClass('x') || $('#squareTR').hasClass('x') && $('#squareM').hasClass('x') && $('#squareBL').hasClass('x')) {
-                alert("X Wins!!!");
-                x_win++;
-                $('#x_win').text(x_win);
+        } else if (turn % 2 != 0) {
+
+            $(this).html('<img src="img/homer2.png" class="pics">');
+            $(this).addClass("disable x");
+            if ($('#squareTL').hasClass('x') && $('#squareTM').hasClass('x') && $('#squareTR').hasClass('x') || $('#squareML').hasClass('x') && $('#squareM').hasClass('x') && $('#squareMR').hasClass('x') || $('#squareBL').hasClass('x') && $('#squareBM').hasClass('x') && $('#squareBR').hasClass('x') || $('#squareTL').hasClass('x') && $('#squareML').hasClass('x') && $('#squareBL').hasClass('x') || $('#squareTM').hasClass('x') && $('#squareM').hasClass('x') && $('#squareBM').hasClass('x') || $('#squareTR').hasClass('x') && $('#squareMR').hasClass('x') && $('#squareBR').hasClass('x') || $('#squareTL').hasClass('x') && $('#squareM').hasClass('x') && $('#squareBR').hasClass('x') || $('#squareTR').hasClass('x') && $('#squareM').hasClass('x') && $('#squareBL').hasClass('x')) {
+                alert("DOH!!! I mean WooHoo!!!");
+                homerWin++;
+                $('#homerWin').text(homerWin);
+                $('.gameboard').text('');
+                $('.gameboard').removeClass('disable');
+                $('.gameboard').removeClass('o');
+                $('.gameboard').removeClass('x');
+                turn = 1;
             }
         }
+        turn++;
+        if (turn >= 9) {
+            alert('Cats Game!!!!')
+        }
+
     });
-    $("#reset").click(function() {
-        $('.gameboard').text('+');
-        $('.gameboard').removeclass('disable');
-        $('.gameboard').removeclass('o');
-        $('.gameboard').removeclass('x');
-        $('.gameboard').removeclass('btn_o');
-        $('.gameboard').removeclass('btn_x');
-        count = 0;
+    $("#startOver").click(function() {
+      	$('.gameboard').text('');
+        $('.gameboard').removeClass('disable');
+        $('.gameboard').removeClass('o');
+        $('.gameboard').removeClass('x');
+        turn = 0;
 
     });
 
